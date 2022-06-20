57abb79d6aaddaea6020ec4f2bc09af5651bb3c6
Karl Skarsholm Risager <karl.risager@gmail.com>
2022-06-20 13:58:13 +0200

    movement (small bugs)

--- chess/game.py
+++ chess/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from chess.pieces import Pawn, Rook, Bishop, Knight, King, Queen
-from .constants import BLACK, WHITE, ROWS, COLS
+from .constants import BLACK, WHITE, BROWN, ROWS, COLS
 from.board import Board
 
 class Game:
@@ -21,35 +21,153 @@ class Game:
     def reset(self):
         self._init()
     
+    def remove(self,piece):
+        self.board.pieces.remove(piece)
+    
     def move(self, piece, row, col):
+        color = piece.color
+        piece_at_pos = self.board.get_piece(row,col)
+        if piece_at_pos != None and piece_at_pos.color != color:
+            self.remove(piece_at_pos)
         self.board.move(piece, row, col)
     
     def get_valid_moves(self, piece):
+        print('in valid_moves')
+        color = piece.color
+        #f = [N,NE,E,SE,S,SW,W,NW]
+        further = [True,True,True,True,True,True,True,True]
         row = piece.row
         col = piece.col
         valid_list = []
-        type = type(piece).__name__
-        if type == 'Pawn':
-            valid_list.append((-1,0))
-            if piece.unmoved:
+        typo = type(piece).__name__
+
+        
+        if typo == 'Pawn':
+            if self.board.get_piece(row-1,col) == None:
+                valid_list.append((-1,0))
+            if piece.unmoved and self.board.get_piece(row-2,col) == None:
                 valid_list.append((-2,0))
             if self.board.get_piece(row-1,col-1)!=None:
                 valid_list.append((-1,-1))
             if self.board.get_piece(row-1,col+1)!=None:
                 valid_list.append((-1,1))
-        elif type == 'Rook':
-            for r in range(ROWS):
-                valid_list.append((r,0))
-                valid_list.append((-r,0))
-            for c in range(COLS):
-                valid_list.append((0,c))
-                valid_list.append((0,-c))
-        elif type == 'Bishop':
-            pass
-        elif type == 'Knight':
-            pass
-        elif type == 'King':
-            pass
-        elif type == 'Queen':
-            pass
+        elif typo == 'Rook':
+
+            for r in range(1,ROWS):
+                if further[4]:
+                    valid_list.append((r,0))
+                    piece_at = self.board.get_piece(row+r,col)
+                    if piece_at != None:
+                        further[4] = False
+
+                if further[0]:
+                    valid_list.append((-r,0))
+                    piece_at = self.board.get_piece(row-r,col)
+                    if piece_at != None:
+                        further[0] = False
+                
+                if further[2]:
+                    valid_list.append((r,0))
+                    piece_at = self.board.get_piece(row,col+r)
+                    if piece_at != None:
+                        further[2] = False
+
+
+                if further[6]:
+                    valid_list.append((0,-r))
+                    piece_at = self.board.get_piece(row,col-r)
+                    if piece_at != None:
+                        further[6] = False
+        elif typo == 'Bishop':
+            for r in range(1,ROWS):
+                if further[3]:
+                    valid_list.append((r,r))
+                    piece_at = self.board.get_piece(row+r,col+r)
+                    if piece_at != None:
+                        further[3] = False
+                if further[1]:
+                    valid_list.append((-r,r))
+                    piece_at = self.board.get_piece(row-r,col+r)
+                    if piece_at != None:
+                        further[1] = False
+                if further[5]:
+                    valid_list.append((r,-r))
+                    piece_at = self.board.get_piece(row+r,col-r)
+                    if piece_at != None:
+                        further[5] = False
+                if further[7]:
+                    valid_list.append((-r,-r))
+                    piece_at = self.board.get_piece(row-r,col-r)
+                    if piece_at != None:
+                        further[7] = False
+        elif typo == 'Knight':
+            valid_list_knight = [(-1,-2),(-2,-1),(-1,2),(-2,1),(1,2),(2,1),(2,-1),(1,-2)]
+            for move in valid_list_knight:
+                valid_list.append(move)
+        elif typo == 'King':
+            valid_list_king = [(1,1),(1,0),(0,1),(-1,-1),(-1,0),(0,-1),(-1,1),(1,-1)]
+            for move in valid_list_king:
+                valid_list.append(move)
+        elif typo == 'Queen':
+            for r in range(1,ROWS):
+
+
+                if further[3]:
+                    valid_list.append((r,r))
+                    piece_at = self.board.get_piece(row+r,col+r)
+                    if piece_at != None:
+                        further[3] = False
+                if further[1]:
+                    valid_list.append((-r,r))
+                    piece_at = self.board.get_piece(row-r,col+r)
+                    if piece_at != None:
+                        further[1] = False
+                if further[5]:
+                    valid_list.append((r,-r))
+                    piece_at = self.board.get_piece(row+r,col-r)
+                    if piece_at != None:
+                        further[5] = False
+                if further[7]:
+                    valid_list.append((-r,-r))
+                    piece_at = self.board.get_piece(row-r,col-r)
+                    if piece_at != None:
+                        further[7] = False
+
+                if further[4]:
+                    valid_list.append((r,0))
+                    piece_at = self.board.get_piece(row+r,col)
+                    if piece_at != None:
+                        further[4] = False
+
+                if further[0]:
+                    valid_list.append((-r,0))
+                    piece_at = self.board.get_piece(row-r,col)
+                    if piece_at != None:
+                        further[0] = False
+                
+                if further[2]:
+                    valid_list.append((0,r))
+                    piece_at = self.board.get_piece(row,col+r)
+                    if piece_at != None:
+                        further[2] = False
+
+
+                if further[6]:
+                    valid_list.append((0,-r))
+                    piece_at = self.board.get_piece(row,col-r)
+                    if piece_at != None:
+                        further[6] = False
+        
+        for move in valid_list:
+            y,x = move
+            r = row + y
+            c = col + x
+            if r > ROWS or r<0 or c > COLS or c<0:
+                valid_list.remove(move)
+            piece_at = self.board.get_piece(r,c)
+            if piece_at != None and piece_at.color == color:
+                valid_list.remove(move)
+        return valid_list
+
+
 
--- main.py
+++ main.py
@@ -19,7 +19,9 @@ def main():
     run = True
     clock = pygame.time.Clock()
     game = Game(WIN)
-    
+    choice_made = False
+    r, c = -1,-1
+    piece = None
     while run:
         clock.tick(FPS)
 
@@ -29,11 +31,19 @@ def main():
             if event.type ==pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_pos_from_mouse(pos)
-                piece = game.board.get_piece(row, col)
-                if piece != None:
-                    c = piece.col
-                    r = piece.row
-                    game.move(piece, r-1,c)
+                if not(choice_made):
+                    piece = game.board.get_piece(row, col)
+                    if piece != None:
+                        choice_made = True
+                        c = piece.col
+                        r = piece.row
+                    break
+                if choice_made:
+                    move = (row-r, col-c)
+                    valid_moves = game.get_valid_moves(piece)
+                    if move in valid_moves:
+                        game.move(piece, row, col)
+                    choice_made = False
         game.update()
 
 
